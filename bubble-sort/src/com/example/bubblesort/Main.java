568e827ef05c6b2fba65f12270e75d417ccd4cbf
Claudio Dornelles <claudio.ramontrejesdornelles@thomsonreuters.com>
2023-05-15 22:47:35 -0300

    Change variables names

--- bubble-sort/src/com/example/bubblesort/Main.java
+++ bubble-sort/src/com/example/bubblesort/Main.java
@@ -15,8 +15,8 @@ public class Main {
   }
 
   private static void bubbleSort(int[] array) {
-    for (var lastSortedIndex = array.length - 1; lastSortedIndex > 0; lastSortedIndex--) {
-      for (var i = 0; i < lastSortedIndex; i++) {
+    for (var lastUnsortedIndex = array.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+      for (var i = 0; i < lastUnsortedIndex; i++) {
         if (array[i] > array[i + 1]) {
           final var temp = array[i + 1];
           array[i + 1] = array[i];
