61afb32dd4941659ac91e4ef2a6ab921aa6aed80
Claudio Dornelles <claudio.ramontrejesdornelles@thomsonreuters.com>
2023-07-23 18:35:24 -0300

    Update algorithm to receive min e max values as parameters

--- counting-sort/src/com/example/CountingSort.java
+++ counting-sort/src/com/example/CountingSort.java
@@ -1,38 +1,33 @@
 package com.example;
 
-import java.util.Arrays;
-
 public class CountingSort {
 
   public static void main(String[] args) {
     final int[] inputArray = {2, 5, 9, 8, 2, 8, 7, 10, 4, 3};
 
-    countingSort(inputArray);
+    countingSort(inputArray, 1, 10);
 
     for (int number : inputArray) {
       System.out.println(number);
     }
   }
 
-  private static void countingSort(int[] inputArray) {
+  private static void countingSort(int[] inputArray, int min, int max) {
     if (inputArray.length < 2) {
       return;
     }
 
-    final var max = Arrays.stream(inputArray).max().orElse(0);
     final int[] countingArray = new int[max];
 
-    for (int number: inputArray) {
-      final var countingArrayIndex = number - 1;
-      countingArray[countingArrayIndex] += 1;
+    for (int i = 0; i < inputArray.length; i++) {
+      countingArray[inputArray[i] - min]++;
     }
 
     int j = 0;
-    for (int i = 0; i < inputArray.length; i++) {
-      if (countingArray[i] > 0) {
-        for (int k = 1; k < countingArray[i] + 1; k++) {
-          inputArray[j++] = i + 1;
-        }
+    for (int i = min; i <= max; i++) {
+      while (countingArray[i - min] > 0) {
+        inputArray[j++] = i;
+        countingArray[i - min]--;
       }
     }
 
